03c4657e6b4be16e681fee7bfc26177eec4b3108
Lyman <m18396531029@163.com>
2020-01-08 18:31:23 +0800

    nsstring

---
+++ LMCFoundation/Classes/Categories/NSString+lmclibs.h
@@ -0,0 +1,17 @@
+//
+//  NSString+lmclibs.h
+//  LMCFoundation
+//
+//  Created by lyman on 2020/1/8.
+//
+
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NSString (lmclibs)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ LMCFoundation/Classes/Categories/NSString+lmclibs.m
@@ -0,0 +1,13 @@
+//
+//  NSString+lmclibs.m
+//  LMCFoundation
+//
+//  Created by lyman on 2020/1/8.
+//
+
+#import "NSString+lmclibs.h"
+
+
+@implementation NSString (lmclibs)
+
+@end
