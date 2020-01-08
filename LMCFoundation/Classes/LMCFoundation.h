422c4760a120eed4641e3fae507c1b7359262c45
Lyman <m18396531029@163.com>
2020-01-08 17:19:40 +0800

    global header

---
+++ LMCFoundation/Classes/LMCFoundation.h
@@ -0,0 +1,14 @@
+//
+//  LMCFoundation.h
+//  Pods
+//
+//  Created by lyman on 2020/1/8.
+//
+
+#ifndef LMCFoundation_h
+#define LMCFoundation_h
+
+#import <LMCFoundation/CategoryHeaders.h>
+
+#endif /* LMCFoundation_h */
+
